c998b13c2f4fb98a21af5f49ce4a9cb7c8a27bda
alnutile <alfrednutile@gmail.com>
2013-10-21 18:31:39 -0400

    relative path for json

--- js/alumni_compare_industry.js
+++ js/alumni_compare_industry.js
@@ -3,9 +3,12 @@
     Drupal.behaviors.alumni_compare_industry = {
         attach: function (context) {
             var arrayData;
+            var base = Drupal.settings.basePath;
+            var url = window.location.host;
+            var protocol = window.location.protocol;
             function drawChart() {
                 $.ajax({
-                    url: "/pse/get/alumni_compare_industry"
+                    url:  protocol + '//' + url + '/' + base +"pse/get/alumni_compare_industry"
                 }).done(function(data) {
                         arrayData = data;
                         $('div.aci-total').append("Total Records " + data['count'] );
